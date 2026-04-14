e8e81f009dc2a90b793003ec9cd61ef0f334e4b4
ANTO <ant.castillos@duocuc.cl>
2026-04-14 14:34:22 -0400

    algo

--- antonella/src/main/java/com/castillo/antonella/Model/Producto.java
+++ antonella/src/main/java/com/castillo/antonella/Model/Producto.java
@@ -1,3 +1,15 @@
-public class Producto {
 
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllargsConstructor
+@NoargsConstructor
+public class Producto 
+{
+    private static final AtomicInteger contador 
 }
